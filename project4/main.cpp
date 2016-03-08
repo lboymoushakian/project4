ceeed389ab7043353a5ec3f34e52cf0e97425562
lboymoushakian <laritalar1997@gmail.com>
2016-03-08 11:26:39 -0800

    Initial Commit

---
+++ project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project4
+//
+//  Created by Lari Boymoushakian on 3/8/16.
+//  Copyright © 2016 Lari Boymoushakian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
